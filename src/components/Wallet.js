e4d7eec82ca1a9adb1ecdd9a80ed7a18d99dedee
bielvbcg <gabrielvbcallegario@gmail.com>
2022-02-07 14:40:41 -0300

    add wallet page integration with api

--- src/components/Wallet.js
+++ src/components/Wallet.js
@@ -1,12 +1,39 @@
-import { useContext, useState } from 'react';
+import { useContext, useState, useEffect } from 'react';
 import { Link, useNavigate } from "react-router-dom"
 import styled from 'styled-components';
 import axios from 'axios';
 import AppContext from '../contexts/AppContext';
 
 export default function Wallet() {
+  const [entrys, setEntrys] = useState(null)
+  const [total, setTotal] = useState(null)
   const { name, token } = useContext(AppContext)
   let navigate = useNavigate();
+
+  useEffect(() => {
+    const config = {
+      headers: {
+        "Authorization": `Bearer ${token}
+      ` }
+    }
+
+    const entrysPromisse = axios.get("http://localhost:5000/entries", config)
+
+    entrysPromisse.then(response => {
+      let sum = 0;
+
+      response.data.map(item => {
+        if (item.type === "in") sum += parseFloat(item.value)
+        else if (item.type === "out") sum -= parseFloat(item.value)
+        console.log(item.value)
+      })
+
+      { response.data !== [] ? setEntrys(response.data) : setEntrys(null) }
+      setTotal(sum)
+    })
+
+    entrysPromisse.catch(error => console.log(error.message))
+  }, [])
 
   return (
     <Main>
@@ -16,9 +43,30 @@ export default function Wallet() {
         <ion-icon name="log-out-outline" onClick={() => navigate("/")}></ion-icon>
       </Title>
 
-      <Register>
-        <span>Não há registros de <br />entrada ou saída</span>
-      </Register>
+      {entrys === null ?
+        <NoEntrys>
+          <span>Não há registros de <br />entrada ou saída</span>
+        </NoEntrys>
+        :
+        <Register>
+
+          <div className="entrys">
+            {entrys.map(entry => (
+              <div className="entry">
+                <div className="date">{entry.date}</div>
+                <div className="description">{entry.description}</div>
+                <div className={`value ${entry.type}`}>{parseFloat(entry.value).toFixed(2)}</div>
+              </div>
+            ))}
+          </div>
+
+          <div className="wallet">
+            <span>SALDO</span>
+            <div className={`total ${total >= 0 ? "in" : "out"}`}>{Math.abs(total).toFixed(2)}</div>
+          </div>
+
+        </Register>
+      }
 
       <ContainerButtons>
 
@@ -61,7 +109,7 @@ const Title = styled.div`
   color: #FFFFFF;
 `
 
-const Register = styled.div`
+const NoEntrys = styled.div`
   display: flex;
   flex-direction: column;
   justify-content: center;
@@ -80,6 +128,90 @@ const Register = styled.div`
 
     color: #868686;
   }
+`
+
+const Register = styled.div`
+  display: flex;
+  flex-direction: column;
+  justify-content: space-between;
+  align-items: center;
+
+  width: 100%;
+  height: 446px;
+  padding-top: 15px;
+  border-radius: 5px;
+
+  background-color: #FFFFFF;
+
+  .entrys {
+    display: flex;
+    flex-direction: column;
+    justify-content: flex-start;
+    align-items: center;
+
+    width: 423px;
+    height: 100%;
+  }
+  
+  .entrys .entry {
+    display: flex;
+    justify-content: space-between;
+    align-items: center;
+
+    font-size: 16px;
+    line-height: 19px;
+  }
+
+  .entrys  .entry .date {
+    width: 48px;
+
+    color: #C6C6C6;
+  }
+
+  .entrys  .entry .description {
+    width: 193px;
+    padding-left: 3px;
+
+    color: #000000;
+  }
+
+  .entrys  .entry .value {
+    width: 62px;
+    text-align: right;
+  }
+  
+  .wallet {
+    display: flex;
+    justify-content: space-between;
+    align-items: center;
+
+    width: 100%;
+
+    padding-right: 11px;
+    padding-bottom: 10px;
+    padding-left: 15px;
+
+    font-size: 17px;
+    line-height: 20px;
+  }
+
+  .wallet span {
+    font-weight: bold;
+    
+    color: #000000;
+  }
+  
+  .wallet .total {
+    text-align: right;
+  }
+  
+    .in {
+      color: #03AC00;
+    }
+  
+    .out {
+      color: #C70000;
+    }
 `
 
 const ContainerButtons = styled.div`
